b45e2cf00e27395b8798f5a2623ee4e66481cda5
veronica <v.blazhko@beauit.com>
2020-02-28 11:38:53 +0200

    refactoring

--- models/normal_conv.py
+++ models/normal_conv.py
@@ -1,31 +1,27 @@
-from keras.models import Sequential
-from keras.layers.normalization import BatchNormalization
-from keras.layers.convolutional import SeparableConv2D
-from keras.layers.convolutional import MaxPooling2D
-from keras.layers.core import Activation
-from keras.layers.core import Flatten
-from keras.layers.core import Dropout
-from keras.layers.core import Dense
 from keras import backend as K
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.convolutional import Conv2D
+from keras.layers.core import Activation
+from keras.layers.core import Dense
+from keras.layers.core import Dropout
+from keras.layers.core import Flatten
+from keras.layers.normalization import BatchNormalization
+from keras.models import Sequential
 
 
-class NeuralNet:
+class NormalConv:
     @staticmethod
-    def build(width, height, depth, classes):
-        # initialize the config along with the input shape to be
-        # "channels last" and the channels dimension itself
+    def build_model(width, height, depth, classes):
         model = Sequential()
         inputShape = (height, width, depth)
         chanDim = -1
 
-        # if we are using "channels first", update the input shape
-        # and channels dimension
         if K.image_data_format() == "channels_first":
             inputShape = (depth, height, width)
             chanDim = 1
 
         # CONV => RELU => POOL
-        model.add(SeparableConv2D(32, (3, 3), padding="same",
+        model.add(Conv2D(32, (3, 3), padding="same",
                                   input_shape=inputShape))
         model.add(Activation("relu"))
         model.add(BatchNormalization(axis=chanDim))
@@ -33,23 +29,23 @@ class NeuralNet:
         model.add(Dropout(0.25))
 
         # (CONV => RELU => POOL) * 2
-        model.add(SeparableConv2D(64, (3, 3), padding="same"))
+        model.add(Conv2D(64, (3, 3), padding="same"))
         model.add(Activation("relu"))
         model.add(BatchNormalization(axis=chanDim))
-        model.add(SeparableConv2D(64, (3, 3), padding="same"))
+        model.add(Conv2D(64, (3, 3), padding="same"))
         model.add(Activation("relu"))
         model.add(BatchNormalization(axis=chanDim))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(0.25))
 
         # (CONV => RELU => POOL) * 3
-        model.add(SeparableConv2D(128, (3, 3), padding="same"))
+        model.add(Conv2D(128, (3, 3), padding="same"))
         model.add(Activation("relu"))
         model.add(BatchNormalization(axis=chanDim))
-        model.add(SeparableConv2D(128, (3, 3), padding="same"))
+        model.add(Conv2D(128, (3, 3), padding="same"))
         model.add(Activation("relu"))
         model.add(BatchNormalization(axis=chanDim))
-        model.add(SeparableConv2D(128, (3, 3), padding="same"))
+        model.add(Conv2D(128, (3, 3), padding="same"))
         model.add(Activation("relu"))
         model.add(BatchNormalization(axis=chanDim))
         model.add(MaxPooling2D(pool_size=(2, 2)))
--- train_model.py
+++ train_model.py
@@ -1,9 +1,10 @@
 import matplotlib
 
+from models.separable_conv import SeparableConv
+
 matplotlib.use("Agg")
 
 from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks import LearningRateScheduler
 from keras.optimizers import Adagrad
 from keras.utils import np_utils
 from sklearn.metrics import classification_report
@@ -14,74 +15,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 import os
-from keras.models import Sequential
-from keras.layers.normalization import BatchNormalization
-from keras.layers.convolutional import SeparableConv2D
-from keras.layers.convolutional import MaxPooling2D
-from keras.layers.core import Activation
-from keras.layers.core import Flatten
-from keras.layers.core import Dropout
-from keras.layers.core import Dense
-from keras import backend as K
-
-
-def build_model(width, height, depth, classes):
-    # initialize the config along with the input shape to be
-    # "channels last" and the channels dimension itself
-    model = Sequential()
-    inputShape = (height, width, depth)
-    chanDim = -1
-
-    # if we are using "channels first", update the input shape
-    # and channels dimension
-    if K.image_data_format() == "channels_first":
-        inputShape = (depth, height, width)
-        chanDim = 1
-
-    # CONV => RELU => POOL
-    model.add(SeparableConv2D(32, (3, 3), padding="same",
-                              input_shape=inputShape))
-    model.add(Activation("relu"))
-    model.add(BatchNormalization(axis=chanDim))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-
-    # (CONV => RELU => POOL) * 2
-    model.add(SeparableConv2D(64, (3, 3), padding="same"))
-    model.add(Activation("relu"))
-    model.add(BatchNormalization(axis=chanDim))
-    model.add(SeparableConv2D(64, (3, 3), padding="same"))
-    model.add(Activation("relu"))
-    model.add(BatchNormalization(axis=chanDim))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-
-    # (CONV => RELU => POOL) * 3
-    model.add(SeparableConv2D(128, (3, 3), padding="same"))
-    model.add(Activation("relu"))
-    model.add(BatchNormalization(axis=chanDim))
-    model.add(SeparableConv2D(128, (3, 3), padding="same"))
-    model.add(Activation("relu"))
-    model.add(BatchNormalization(axis=chanDim))
-    model.add(SeparableConv2D(128, (3, 3), padding="same"))
-    model.add(Activation("relu"))
-    model.add(BatchNormalization(axis=chanDim))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-
-    # first (and only) set of FC => RELU layers
-    model.add(Flatten())
-    model.add(Dense(256))
-    model.add(Activation("relu"))
-    model.add(BatchNormalization())
-    model.add(Dropout(0.5))
-
-    # softmax classifier
-    model.add(Dense(classes))
-    model.add(Activation("softmax"))
-
-    # return the constructed network architecture
-    return model
 
 
 # construct the argument parser and parse the arguments
@@ -97,8 +30,8 @@ INIT_LR = 1e-2
 BS = 32
 
 # initialize our CancerNet config and compile it
-model = build_model(width=48, height=48, depth=3,
-                    classes=2)
+model = SeparableConv.build_model(width=48, height=48, depth=3,
+                                  classes=2)
 opt = Adagrad(lr=INIT_LR, decay=INIT_LR / NUM_EPOCHS)
 model.compile(loss="binary_crossentropy", optimizer=opt,
               metrics=["accuracy"])
